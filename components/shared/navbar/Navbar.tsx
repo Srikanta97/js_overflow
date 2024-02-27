f0f50267747e4a91264551106dfa4156a8711bab
Srikanta <srikanta.aug97@gmail.com>
2024-02-27 22:49:20 +0530

    css cleanup

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -37,7 +37,9 @@ const Navbar = () => {
             }}
           />
         </SignedIn>
-        <MobileNav />
+        <div className="cursor-pointer">
+          <MobileNav />
+        </div>
       </div>
     </nav>
   );
